2821e179f541d856e372fcb94d2ff15081a4f73f
PavelD <nagellstev@ya.ru>
2022-12-28 14:29:10 +0400

    First commit

---
+++ Practical_Task_Dev_and_Build_Tools/Program.cs
@@ -0,0 +1,52 @@
+﻿///This program takes a sequence of symbols (string) as arguments from the command line 
+///and prints the maximum number of unequal consecutive characters per line to the console.
+
+using System;
+using System.Security.Permissions;
+using CalculatorProgram;
+using StringLibrary;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool endApp = false;
+            Console.WriteLine("Practical Task: Chapter Development and Build Tools\r");
+            Console.WriteLine("-----------\n");
+            StringProcessing strPr = new StringProcessing();
+            while (!endApp)
+            {
+                string Input = "";
+                int result = 0;
+
+                while (Input == "")
+                {
+                    Console.WriteLine("Type string, press Enter\n");
+                    Input = Console.ReadLine();
+                }
+
+                try
+                {
+                    result = strPr.DoProcessing(Input);
+                    Console.WriteLine("Maximum number of unequal consecutive characters in this line is \n" + result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something Wrong!\n" + e.Message);
+                }
+                Console.WriteLine("----------------------\n");
+                Console.WriteLine("Press 'n' and 'Enter' to exit or other key to continue.\n");
+                if (Console.ReadLine() == "n")
+                {
+                    endApp = true;
+                }
+                Console.WriteLine("\n");
+            }
+            strPr.Finish();
+            return;
+        }
+    }
+
+}
---
+++ StringLibrary/StringProcessing.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Formats.Asn1;
+using System.Xml;
+
+namespace StringLibrary
+{
+    public class StringProcessing
+    {
+        
+        JsonWriter writer;
+        public StringProcessing()
+        {
+            StreamWriter logFile = File.CreateText("StringProcessing.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Newtonsoft.Json.Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("String Processings");
+            writer.WriteStartArray();
+        }
+        
+        public int DoProcessing(string inputStr)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("Input String");
+            writer.WriteValue(inputStr);
+            int result = 0;
+            int currNumberOfUneqCh = 0;
+
+            for (int i = 1; i < inputStr.Length; i++)
+            {
+                if (inputStr[i] != inputStr[i - 1])
+                {
+                    currNumberOfUneqCh++;
+                }
+                else
+                {
+                    if (result < currNumberOfUneqCh)
+                    {
+                        result = currNumberOfUneqCh;
+                    }
+                    currNumberOfUneqCh = 0;
+                }
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+        
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+        
+    }
+}
